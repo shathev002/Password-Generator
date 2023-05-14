a6237e65239d33e31e6f0149e55e186ac133af09
Sharanya Thevakumaran <sharanyat@hotmail.com>
2023-05-13 23:11:19 -0400

    updated javascript and readme files

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,4 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
-function writePassword() {
-  var prompts =  showPrompt(); // will be true or false if follow the steps correctly and move on to generate password if true 
-  var passwordText = document.querySelector("#password");
-
-  if(prompts) {
-    var finalpassword = generatePassword();
-    passwordText.value = finalpassword;
-
-  } else {
-    passwordText.value="";
-
-  }
-
- }
-
+//variables set for length of password, selection criteria, uppcase, lowercase, number and special characters.
 var length = '';
 var selection = [];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -27,10 +8,32 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var special = ['!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '/', ';', ':', '<', '>', '=', '?', '@', '[', ']', '_', '{', '}', '|', '~',];
 
+
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  var prompts =  showPrompt(); 
+  var passwordText = document.querySelector("#password");
+
+// will be true or false if follow the steps correctly and move on to generate password if true 
+  if(prompts) {
+    var finalpassword = generatePassword();
+    passwordText.value = finalpassword;
+
+  } else {
+    passwordText.value=""; // if don't follow prompts correctly password will be blank
+
+  }
+
+ }
+
+
+
 function generatePassword() {
  var password = '';
  for(var i = 0; i < length; i++) {
-  var randomkey = Math.floor(Math.random() * selection.length);
+  var randomkey = Math.floor(Math.random() * selection.length); //use Math.floor to convert calculation into a whole number 
   password = password + selection[randomkey];
 
  }
@@ -41,15 +44,15 @@ function generatePassword() {
 
 function showPrompt() {
   selection = [];
-
+// use parseInt to convert into a string and then get an integer to calculate length of password
   length = parseInt(prompt("How many characters would you like your password to be? Must be between 8 and 128 characters"));
 
   if(length < 8 || length > 128) {
-    alert("Password must be between 8 and 128 characters. Please try again");  // To check if meet length condition
+    alert("Password must be between 8 and 128 characters. Please try again");  // To check if meet length condition first before other criteria
     return false;
   }
   if(confirm("Would you like your password to have lowercase letters?")) {
-    selection = selection.concat(lowerCase);
+    selection = selection.concat(lowerCase); //used concat to combine strings
 
   }
 
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Password-Generator
+
+## Description
+
+Created a password generator to allow the user to create a random password based on critera selected. 
+
+A series of prompts will be shown to the user to select a password that meets the conditions specified. 
+
+Password must be between 8 and 128 characters and have at least one character type which includes a lowercase, uppercase,
+numeric, or special character. 
+
+If password length not met, a prompt will display instructing the user to try again. 
+
+Once length is confirmed, other criteria will be shown and a final password will be created. 
+
+Applied all the javascript skills learned this week to create functions that generate a password. 
+
+Learned how to use if statements and for loops to create conditions that will stop the process or complete it and return a value, in this case a password. 
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+You can refer to the javascript file to access the code for this project. 
+
+To view the actual webpage please refer to the following url link.
+
+https://shathev002.github.io/Portfolio/
+
+You can also refer to the screenshot below. 
+
+![alt text](./assets/web%20page%20screenshot.png)
+
+## Credits
+
+https://www.w3schools.com/ was used for reference regarding CSS and HTML topics. 
+
+## License
+
+N/A
